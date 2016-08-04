65b830947743ad1cd9ca94dd622889a38103fba5
caiocmsouza <caiocesar.msouza@gmail.com>
2016-08-04 16:09:58 -0300

    aula2

---
+++ header.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+	<?php $home = get_template_directory_uri(); ?>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="<?= $home ?>/assets/css/reset.css">
+	<link rel="stylesheet" href="<?= $home; ?>/style.css">
+
+
+	<?php	wp_head(); ?>
+</head>
+<body>
--- index.php
+++ index.php
@@ -1,13 +1,25 @@
-<!doctype html>
-<html>
-<head>
-	<meta charset="utf-8">
-</head>
-<body>
+<?php get_header(); ?>
+<main class="home-main">
+	<div class="container">
 
-	<h1>Bem vindo ao Maluras!</h1>
-	
-</body>
+		<?php if( have_posts() ) { ?>
+			<ul class="imoveis-listagem">
+			<?php while( have_posts() ) {
+					the_post(); ?>
+
+				<li class="imoveis-listagem-item">
+					<?php the_post_thumbnail(); ?>
+
+					<h2><?php the_title(); ?></h2>
+
+					<p><?php the_content(); ?></p>
+				</li>
+
+			<?php } ?>
+			</ul>
+		<?php	} ?>
+	</div>
+</main>
 
 
-</html>
+<?php get_footer(); ?>
